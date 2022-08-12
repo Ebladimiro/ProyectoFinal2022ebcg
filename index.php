37d2c173e5baddcd1c9f3c3e05240dfb23e6ccd4
Ebladimiro <elcapriel@gmail.com>
2022-08-12 11:56:34 -0600

    tarea 2

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="diseño1.css">
+    <link rel="stylesheet" href="diseño2.css">
+    <title>POLLO CAMPERO</title>
+</head>
+<table width="100%">
+ <tr>
+ <td colspan="12" align="center">
+ <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+ <div class="carousel-indicators">
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4  "></button>
+ </div>
+ <div class="carousel-inner">
+ <div class="carousel-item active">
+ <img src="image/pollo camperoi.jpg" class="d-block w-90" alt="...">
+ </div>
+ <div class="carousel-item">
+      <img src="image/pollo campero.jpg" class="d-block w-90" alt="...">  
+      </div>
+      <div class="carousel-item">
+      <img src="image/pollo campero 3.jpg" class="d-block w-90" alt="...">
+      </div>
+      <div class="carousel-item">
+      <img src="image/pollito.jpg" class="d-block w-90" alt="...">
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+      </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span><br>
+  </button><br>
+  </div><br><br>
+      <table style="border-collapse: collapse; font-family: 'Times New Roman', Times, serif;" border="1px" width="100%">
+      <tbody><tr>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #ffe100;"><a href="productos.php" style="text-decoration: none; color: rgb(28, 27, 23);"><b>PRODUCTOS</b></a></td>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #00aeff;"><a href="tiendas.php" style="text-decoration: none; color: rgb(28, 27, 23);"><b>TIENDAS</b></a></td>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #0c5bb0;"><a href="contactenos/registro.php" style="text-decoration: none; color: rgb(30, 28, 27);"><b>CONTACTENOS</b></a></td>
+    </tr>
+  </tbody></table><br><br>
+        </tr>
+        <td colspan="12" align="left"><br>
+                <h3 style="color: orange;"><b>NUESTRAS OFERTAS DE MES</b></h3>
+            </td>
+    </table><br>
+    <div class="contenedor">
+        <section class="a" align="center"><a href="image/logo.png"><img src="image/logo.png" width="330px" height="300px"></a></section>
+        <section class="b" align="center"><img src="image/img1.png" width="300px" height="250px"></section>
+        <section class="c" align="center"><img src="image/img2.png" width="300px" height="250px"></section>
+        <section class="d" align="center"><img src="image/img3.jpg" width="300px" height="200px"></section>
+    </div>
+    <table align="center" width="100%">
+        <tr style="background-color: orange;">
+            <td colspan="3" align="left">
+                <h6 style="color: withe;"><b>PBX: XXXXX-XXXXX</b> </h6>
+            </td>
+            <td colspan="3" align="right">
+                <h6 style="color: left;"><b>Dirección: XXXXX-XXXXX-XXXX</b></h6>
+            </td>
+            <td colspan="3" align="right">
+                <h6><b>Carné: 2020-40180</b></h6>
+            </td>
+            <td colspan="4">.</td>
+            <td colspan="3" align="right">
+                <h6><b>Nombre: Edwin Bladimiro Capriel Garcia</b></h6>
+            </td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ productos.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRODUCTOS</title>
+    <link rel="stylesheet" href="produ.css">
+</head>
+<body>  
+
+    <div class="container">
+
+        <div class="card">
+            <img src="image/prod1.png">
+            <h3>BANQUETE DE ALITAS</h3>
+            <p> ALITAS BAÑADAS EN NUESTRA RICA BARBACOA</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod2.png">
+            <h3>PIZZA</h3>
+            <p> PIZZAS PERSONALES Y FAMILIARES</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod3.png">
+            <h3>COMBO PARA TRES PERSONAS</h3>
+            <p> PRUEBA NUESTROS GRANDIOSOS COMBOS</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod4.png">
+            <h3>ENSALADAS</h3>
+            <p> PRUEBA NUESTRAS GRANDIOSAS SALADAS</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod5.png">
+            <h3>DESAYUNOS</h3>
+            <p> PRUEBA NUESTROS RICOS DESAYUNOS </p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod6.png">
+            <h3>MENU INFANTIL</h3>
+            <p> PRUEBA NUESTROS MENUS INFANTILES</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod7.png">
+            <h3>DESAYUNOS</h3>
+            <p> PRUEBA NUESTROS DESAYUNOS DE TIEMPO LIMITADO</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod8.png">
+            <h3>BEBIDAS Y POSTRES</h3>
+            <p> TAMBIEN PRUEBA NUESTROS INCREIBLES POSTRES</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod9.png">
+            <h3>COMBO PERSONAL</h3>
+            <p> Lorem ipsum dolor sit amet, consetur adipsion elit</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod10.png">
+            <h3>SUPER COMBO </h3>
+            <p> PRUEBA NUESTRO ASOMBROSO COMBO DE PECHUGITAS</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod11.png">
+            <h3>BANQUETE DE PRECHUGUITAS</h3>
+            <p> PRUEBA NUESTRAS CRUJIENTES PECHUGUITAS</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        <div class="card">
+            <img src="image/prod12.png">
+            <h3>BANQUETE 30</h3>
+            <p> PRTUEBA NUESTRO NUEVO Y MEJORADO BANQUETE DE ALITAS</p>
+            <a href="#"><b>ORDENAR</b></a>
+        
+        </div>
+        
+
+</body>
+</html>
+
+
+
